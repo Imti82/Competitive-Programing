f86272f7bd142d703af816595aea246be325670f
Imti <imtinanwasir@gmail.com>
2025-03-08 23:16:33 +0600

    Add files via upload

---
+++ str_length.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string S="Level";
+    string T="One";
+    cout<<S.length()<<" "<<T.length()<<endl;
+    cout<<S+" "+T<<endl;
+    return 0;
+}
