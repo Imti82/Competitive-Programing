abe34c7661113d926c59cb9ec1d0c0bab9dc9a25
Imti <imtinanwasir@gmail.com>
2025-06-18 00:50:06 +0600

    Add files via upload

---
+++ Way_too_long_word.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    while (n--) {
+        string word;
+        cin >> word;
+        
+        if (word.length() > 10) {
+            cout << word.front() << word.length() - 2 << word.back() << endl;
+        } else {
+            cout << word << endl;
+        }
+    }
+
+    return 0;
+}
